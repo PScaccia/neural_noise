47ada91c636cd43d060256b88b96d85cd99bf881
Paolo Scaccia <scaccia.paolo@gmail.com>
2024-10-07 17:35:04 +0200

    Added flatted-top Von mises function as Tuning curves

--- network.py
+++ network.py
@@ -6,23 +6,35 @@ Created on Thu Oct  3 10:09:15 2024
 @author: paoloscaccia
 """
 import numpy as np
+import sys
 
+def generate_tuning_curve( function = 'vm', 
+                           center = 2, A = 2, width = 1.6, flatness = 0.1, b = 0.1):
+    # Reference: https://swindale.ecc.ubc.ca/wp-content/uploads/2020/09/orientation_tuning_curves.pdf
+    
+    if function == 'vm':
+        return lambda x : A*( ( (np.exp( np.cos(x - center)/width) - np.exp(-1/width) ) /  (np.exp(1/width) - np.exp(-1/width)) ) + b )
+    elif function == 'fvm':
+        return lambda x: A*np.exp( (np.cos((x - center - abs(flatness)*np.sin(2*(x - center)))) - 1)/width )
+        # return lambda x:  A*np.exp(  width*( flatness*np.sin( x - center ) - 1 )  )
+    else:
+        sys.exit("Unknown function")
 
-
-def generate_tuning_curve( center = 0.3, w = 0.5, a = 0.4, b = 0.1, scale = 1):
-    return lambda x : ( ( (np.exp( np.cos(x - center)/w) - np.exp(-1/w) ) /  (np.exp(1/w) - np.exp(-1/w)) ) + b )*scale
-
+def generate_variance_matrix( V, rho, D = 2):
+    if D != 2:
+        sys.exit("Not implemented yet!")
+    
+    return V*np.array( [[1, rho], [rho, 1]])
 
 class NeuralSystem(object):
     
     def __init__( self, tuning_curves, covariance_matrix, N_trial = 1000):
         
-        def neural_dynamics(x, mu_functions, cov_matrix, N_trial):
-            return np.random.multivariate_normal(  [ f(x) for f in mu_functions ], # Average Vector
+        def neural_dynamics(t, mu_functions, cov_matrix, N_trial):
+            return np.random.multivariate_normal(  [ f(t) for f in mu_functions ], # Average Vector
                                                    cov_matrix,                     # Covariance Matrix
                                                    size = N_trial )
         #.astype(int)
-                        
         self.mu         = tuning_curves
         self.covariance = covariance_matrix
         self.N_trial    = N_trial
--- plot_tools.py
+++ plot_tools.py
@@ -8,18 +8,19 @@ Created on Thu Oct  3 13:10:10 2024
 
 import matplotlib.pyplot as plt
 import numpy as np
-
+from network import NeuralSystem, generate_tuning_curve, generate_variance_matrix
 
 def plot_simulation( system, stimolus ):
     
     markers = ['o','+','s','^','*','v']
         
+    fig, axs = plt.subplots(1,2,figsize = (14,6))
     
     # Plot Trials per Stimolous
     for i,t in enumerate(stimolus):
         data = system.neurons(t)
         j = i%len(markers)
-        plt.scatter(data[:,0],data[:,1],
+        axs[0].scatter(data[:,0],data[:,1],
                     alpha=0.5,
                     marker=markers[ j ],
                     label = r"{:.2f}".format(  t/np.pi ) 
@@ -28,17 +29,53 @@ def plot_simulation( system, stimolus ):
         
     # Plot Signal Manifold
     theta = np.linspace(stimolus[0], stimolus[-1] + np.pi,1000)
-    plt.plot( list( map( system.mu[0], theta)), 
+    axs[0].plot( list( map( system.mu[0], theta)), 
                  list( map( system.mu[1], theta)), 
                  c = 'black',
                  lw = 1 )
     
-    plt.xlabel('Response 1', weight='bold',size=18)
-    plt.ylabel('Response 2', weight='bold',size=18)
-    plt.legend( title = r'Theta ($\pi$)')
+    axs[0].set_xlabel('Response 1', weight='bold',size=18)
+    axs[0].set_ylabel('Response 2', weight='bold',size=18)
+    axs[0].legend( title = r'Theta ($\pi$)')
     
+    plot_tuning_curves(*system.mu, ax = axs[1])
     plt.show()
     
-
     
     return 
+
+def plot_tuning_curves(mu1, mu2, ax = None):
+
+    theta = np.linspace(0,2*np.pi,1000)
+    
+    if ax is None:
+        plt.plot(theta, mu1(theta), label = 'Neuron 1')
+        plt.plot(theta, mu2(theta), label = 'Neuron 2')
+        plt.xlabel(r"$\mathbf{\theta} [\pi]$",size=15)
+        plt.ylabel("Activity",size=15,weight='bold')
+    else:
+        ax.plot(theta, mu1(theta), label = 'Neuron 1')
+        ax.plot(theta, mu2(theta), label = 'Neuron 2')
+        ax.set_xlabel(r"$\mathbf{\theta}$ (rad)",size=15)
+        ax.set_ylabel("Activity",size=15,weight='bold')
+    plt.show()    
+    return
+
+def run_simulation( peak_shift = 0.3, A = 1, width = 1, flatness = 1, function = 'vm', V = 1e-2, rho = 0.7):
+    
+    # System params
+    first_peak = 2.1
+    mu1 = generate_tuning_curve( function = function, width=width, center = first_peak, A = A)    
+    mu2 = generate_tuning_curve( function = function, width=width, center = first_peak + peak_shift, A = A )
+    sigma = generate_variance_matrix(V, rho )
+    
+    plot_tuning_curves(mu1, mu2)
+    
+    # Define Neural Network
+    system = NeuralSystem([mu1, mu2], sigma, N_trial = 1e4)
+    
+    # Plot Simulation
+    stimuli = np.pi*np.array( [0, 1/3, 0.5, 2/3, 0.8,1,1.2,1+1/3,1.6666])
+    plot_simulation(system, stimuli)
+    
+    return system
